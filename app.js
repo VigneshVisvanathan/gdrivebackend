9cdb383ce0f921be2fe944fbf3f260642678731d
VigneshVisvanathan <59162869+VigneshVisvanathan@users.noreply.github.com>
2020-08-01 16:22:41 +0530

    'linkchanges'

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'public')));
-const corsOptions = {
-  origin: 'http://localhost:4200',
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
